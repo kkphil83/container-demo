c18705d55074fe0850fb8b69b7aea91058415618
kkphil83 <kkphil83@gmail.com>
2023-05-22 18:00:49 +0900

    222

--- script/02_1_create_demo_vm.sh
+++ script/02_1_create_demo_vm.sh
@@ -7,6 +7,10 @@ vm_yaml="./yaml/rhel8-demo-vm.yaml"
 # 프로젝트 설정
 oc project $project_name
 
+# VM yaml 파일내용 수정
+pvc_name=`oc get pvc -n openshift-virtualization-os-images | grep rhel8 | cut -d ' ' -f1`
+sed -i 's/PVC_NAME/$pvc_name/g' $vm_yaml
+
 # VM 생성
 oc apply -f $vm_yaml -n $project_name
 

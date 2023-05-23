5f847ae566f768a725ed2470ff2a1c9613ad1c45
kkphil83 <kkphil83@gmail.com>
2023-05-23 17:52:15 +0900

    o

---
+++ script/00_container_workshop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 베어메탈 머신 수 5개로 증가
+mcs_name=`oc get machineset -n openshift-machine-api | grep metal | cut -d ' ' -f1`
+oc scale --replicas=5 machineset $mcs_name -n openshift-machine-api
+
+sleep 20m
+
+echo '## machineset STATUS ##'
+oc get machineset -n openshift-machine-api
+echo ' '
+
+# 스크립트 순차 실행
+sh 01_create_demo_vm.sh
+sh 02_new-user-project.sh
+sh 08_datavolume-cloner_role.sh
+sh 09_pvc_clone.sh
+sh 10_user_vm_create.sh
+sh 11_project_rabc_settings.sh
+sh 12_podman_httpd_svc.sh
+
+# 웹 터미널 오퍼레이터 설치
+oc apply -f ./yaml/web-terminal-operator.yaml 
--- script/10_user_vm_create.sh
+++ script/10_user_vm_create.sh
@@ -2,6 +2,7 @@
 
 # YAML 파일 경로
 yaml_file="./yaml/rhel8-clone.yaml"
+svc_yaml_file="./yaml/ssh-svc.yaml"
 
 # 네임스페이스 이름 변수
 namespace_prefix="user"
@@ -12,5 +13,6 @@ for ((suffix=1; suffix<=20; suffix++)); do
     namespace="${namespace_prefix}${suffix}-${namespace_suffix_common}"
     echo "Applying YAML for namespace: $namespace"
     oc apply -n "$namespace" -f "$yaml_file"
+    oc apply -n "$namespace" -f "$svc_yaml_file"
 done
 

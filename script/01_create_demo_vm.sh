df8ebae20964990ab8fc243809d449596845f836
kkphil83 <kkphil83@gmail.com>
2023-05-24 15:18:11 +0900

    11

--- script/01_create_demo_vm.sh
+++ script/01_create_demo_vm.sh
@@ -23,11 +23,12 @@ pvc_name=`oc get pvc -n openshift-virtualization-os-images | grep rhel8 | cut -d
 sed -i "s/PVC_NAME/$pvc_name/g" $vm_yaml
 
 # ssh key 생성 생성
-sudo -i yum install -y expect
-sh ./ssh-keygen.sh
+#sudo -i yum install -y expect
+#sh ./ssh-keygen.sh
 #oc create secret generic rhel8-vm-ssh-key --from-file=ssh-privatekey=~/.ssh/id_rsa --from-file=ssh-publickey=~/.ssh/id_rsa.pub
 
 # cloud init 스크립트 시크릿 생성
+oc project  $project_name
 oc create secret generic rhel8-vm-secret --from-file=userdata=./yaml/startup.sh
 
 #startup_script=`cat ./yaml/startup.sh | base64 -w0`
